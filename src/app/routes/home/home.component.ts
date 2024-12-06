8aba0e42f3e383db94411a581322dee62faf13f0
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-12-06 01:57:38 -0300

    FEAT: Página de Login Com conexão funcionando para validar email/usuário

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface User {
+  id: string;
+  username: string;
+  email: string;
+  senha: string;
+  dtCriacao: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HttpClientModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  private apiUrl = 'http://localhost:3000/api/users';  // URL do seu servidor Node.js
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers().subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log("DEU RUIM");
+      }
+    );
+  }
+
+
+
+  // Método para buscar todos os usuários
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiUrl);
+  }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Observable, take, tap } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  // #region ==========> PROPERTIES <==========
+
+  // #region PRIVATE
+  private readonly USERS_URL: string = `${environment.apiUrl}/users`;
+  private readonly HTTP_HEADERS: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+  // #endregion PRIVATE
+
+  // #region PUBLIC
+  // [...]
+  // #endregion PUBLIC
+
+  // #endregion ==========> PROPERTIES <==========
+
+
+  // #region ==========> INITIALIZATION <==========
+  constructor( private _httpClient: HttpClient ) { }
+  // #endregion ==========> INITIALIZATION <==========
+
+
+  // #region ==========> SERVICE METHODS <==========
+
+  // #region PREPARATION
+  // [...]
+  // #endregion PREPARATION
+
+  // #region GET
+  public validateEmail(username_email: string): Observable<string> {
+    const params = new HttpParams().set('username_email', username_email);
+
+    const url = `${this.USERS_URL}/validateUsernameEmail`
+
+    return this._httpClient.get<string>(url, {
+      'headers': this.HTTP_HEADERS,
+      'params': params
+    });
+  }
+  // #endregion GET
+
+  // #region POST
+  // [...]
+  // #endregion POST
+
+  // #region DELETE
+  // [...]
+  // #endregion DELETE
+
+  // #endregion ==========> SERVICE METHODS <==========
+
+
+  // #region ==========> UTILS <==========
+  // [...]
+  // #endregion ==========> UTILS <==========
+
 }
