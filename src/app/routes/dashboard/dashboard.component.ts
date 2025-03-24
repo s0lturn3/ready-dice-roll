0d2625bad80b367a9955db7f991e32e39491dc8d
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2025-03-24 01:45:31 -0300

    ajustes visuais e menu lateral feito

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { AuthService } from '../../shared/services/auth.service';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { SkillTreeComponent } from '../../shared/components/skill-tree/skill-tree.component';
 
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -30,20 +29,15 @@ export class DashboardComponent implements OnInit {
   // #endregion ==========> PROPERTIES <==========
 
 
-  // #region ==========> FORM CONFIG <==========
-  // [...]
-  // #endregion ==========> FORM CONFIG <==========
-
-
-  constructor(
-    private _authService: AuthService
-  ) { }
+  constructor(private _authService: AuthService) { }
 
   ngOnInit(): void {
     this._authService.jwttest().subscribe({
       next: response => { console.log(response); },
       error: error => { console.log(error); }
     });
+
+    this.adjustSidebar();
   }
 
 
@@ -69,7 +63,42 @@ export class DashboardComponent implements OnInit {
 
 
   // #region ==========> UTILS <==========
-  // [...]
+  private adjustSidebar(): void {
+    const sidebar = document.querySelector(".sidebar") as HTMLElement;
+    const sidebarToggler = document.querySelector(".sidebar-toggler");
+    const menuToggler = document.querySelector(".menu-toggler");
+
+    // Ensure these heights match the CSS sidebar height values
+    let collapsedSidebarHeight = "56px"; // Height in mobile view (collapsed)
+    let fullSidebarHeight = "calc(100vh - 32px)"; // Height in larger screen
+
+    // Toggle sidebar's collapsed state
+    sidebarToggler!.addEventListener("click", () => {
+      sidebar!.classList.toggle("collapsed");
+    });
+
+    // Update sidebar height and menu toggle text
+    const toggleMenu = (isMenuActive: boolean) => {
+      sidebar!.style.height = isMenuActive ? `${sidebar!.scrollHeight}px` : collapsedSidebarHeight;
+      menuToggler!.querySelector("span")!.innerText = isMenuActive ? "close" : "menu";
+    }
+
+    // Toggle menu-active class and adjust height
+    menuToggler!.addEventListener("click", () => {
+      toggleMenu(sidebar!.classList.toggle("menu-active"));
+    });
+
+    // (Optional code): Adjust sidebar height on window resize
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 1024) {
+        sidebar!.style.height = fullSidebarHeight;
+      } else {
+        sidebar!.classList.remove("collapsed");
+        sidebar!.style.height = "auto";
+        toggleMenu(sidebar!.classList.contains("menu-active"));
+      }
+    });
+  }
   // #endregion ==========> UTILS <==========
 
 }
