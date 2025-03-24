23378e90fe8b99ed77d269fdc480c1720f295837
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-03-23 23:50:43 -0300

    troca de máquina

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { SkillTreeComponent } from '../../shared/components/skill-tree/skill-tree.component';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     NgxGraphModule,
-    SkillTreeComponent
+    SkillTreeComponent,
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
+
+  // #region ==========> PROPERTIES <==========
+
+  // #region PRIVATE
+  // [...]
+  // #endregion PRIVATE
+
+  // #region PUBLIC
+  public menuOpen: boolean = false;
+  // #endregion PUBLIC
+
+  // #endregion ==========> PROPERTIES <==========
+
+
+  // #region ==========> FORM CONFIG <==========
+  // [...]
+  // #endregion ==========> FORM CONFIG <==========
+
 
   constructor(
     private _authService: AuthService
@@ -25,5 +45,31 @@ export class DashboardComponent implements OnInit {
       error: error => { console.log(error); }
     });
   }
+
+
+  // #region ==========> API METHODS <==========
+
+  // #region GET
+  // [...]
+  // #endregion GET
+
+  // #region POST
+  // [...]
+  // #endregion POST
+
+  // #region PUT
+  // [...]
+  // #endregion PUT
+
+  // #region DELETE
+  // [...]
+  // #endregion DELETE
+
+  // #endregion ==========> API METHODS <==========
+
+
+  // #region ==========> UTILS <==========
+  // [...]
+  // #endregion ==========> UTILS <==========
 
 }
