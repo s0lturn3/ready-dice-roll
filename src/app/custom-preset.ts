5181a9bc11cffbec383d4d4c474ade85b7073ccf
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-09-24 13:40:23 -0300

    início da reescrita da tela inicial

--- src/app/custom-preset.ts
+++ src/app/custom-preset.ts
@@ -4,17 +4,17 @@ import Aura from '@primeng/themes/aura';
 export const CustomPreset = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '{indigo.50}',
-      100: '{indigo.100}',
-      200: '{indigo.200}',
-      300: '{indigo.300}',
-      400: '{indigo.400}',
-      500: '{indigo.500}',
-      600: '{indigo.600}',
-      700: '{indigo.700}',
-      800: '{indigo.800}',
-      900: '{indigo.900}',
-      950: '{indigo.950}'
+      50: '#f0f3fe',
+      100: '#dde3fc',
+      200: '#c3cffa',
+      300: '#9ab1f6',
+      400: '#6a89f0',
+      500: '#4560e9',
+      600: '#3243de',
+      700: '#2a31cb',
+      800: '#282aa5',
+      900: '#252a83',
+      950: '#1b1c50',
     },
 
     colorScheme: {
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-    selector: 'app-header',
-    imports: [
-        RouterModule
-    ],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+  selector: 'app-header',
+  imports: [
+    RouterModule,
+    ButtonModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
 
